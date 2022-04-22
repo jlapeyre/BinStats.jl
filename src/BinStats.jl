0f1a6425d196dc2bcb5b220b50020222949be831
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-04-22 13:01:03 -0400

    Add BitMatrix support and printing, etc.

--- src/BinStats.jl
+++ src/BinStats.jl
@@ -2,45 +2,106 @@ module BinStats
 import JLD2
 import Random
 
-export readhex, writehex
+export readhex, writehex, bitmatrix, parsehexline, bitprint
 export Bin64
-export revbits
 
-struct Bin64
+struct Bin64 <: Unsigned
     x::UInt64
 end
 
+@inline Base.UInt64(b::Bin64) = b.x
+
 function Base.show(io::IO, b::Bin64)
     print(io, string(b.x, base=2, pad=64))
-#    print(io, reverse(string(b.x, base=2, pad=64)))
 end
 
 function Base.show(io::IO, v::Vector{<:Bin64})
-    for x in v
+    n = length(v)
+    for (i, x) in enumerate(v)
         show(io, x)
-        println(io)
+        print(io)
+        if i != n
+            println(io)
+        end
     end
 end
 
 Random.rand(rng::Random.AbstractRNG, ::Random.SamplerType{Bin64}) = Bin64(rand(rng, UInt64))
 
 # First bit is leftmost, that is MSB
-function Base.getindex(b::Bin64, i::Integer)
+@inline function Base.getindex(b::Bin64, i::Integer)
     return Bool((b.x >> (64 - i)) & 1)
 end
 
-function Base.getindex(b::Bin64, inds::AbstractVector{<:Integer})
+@inline function Base.getindex(b::Bin64, inds::AbstractVector{<:Integer})
     return BitArray(b[i] for i in inds)
 end
 
+"""
+    bitmatrix(m::AbstractVector{<:Union{UInt64,Bin64}})
+
+Convert `m` to a `64 x n` `BitMatrix`, where `n == length(m)`.
+
+In contrast to the builtin construction, the semantic order of the bits in the original array are
+preserved. In otherwords each `UInt64` is bit-reversed befor copying into the constructed
+`BitMatrix`.
+
+Furthermore, the output is wrapped in a `Transpose` before returning. Thus, the output has the same
+semantics of `Vector{Bin64}` when treated as a matrix. If the input vector `m` was read with
+`readhex`, then indexing into the output `BitMatrix` will access the bits in the order they
+appear in the input file.
+
+You may get better performance or flexibility by converting the `Transpose` object to a concrete
+`BitMatrix` like this: `collect(bitmatrix(v))`.
+"""
+bitmatrix(m::AbstractVector{UInt64}) = _bitmatrix(m, bitreverse)
+bitmatrix(m::AbstractVector{Bin64}) = _bitmatrix(m, x -> bitreverse(x.x))
+@inline function _bitmatrix(m, revfunc)  # @inline really necessary here
+    ba = BitArray(undef, (64, length(m)))
+    copy!(ba.chunks, revfunc.(m))
+    return transpose(ba)
+end
+
+"""
+    bitprint([io::IO], v::AbstractVector{Bool})
+    bitprint([io::IO], m::AbstractMatrix{Bool})
+
+Print `v` or `m` in a compact form, with adjacet zeros and ones.
+"""
+bitprint(x::AbstractArray{Bool}) = bitprint(Base.stdout, x)
+bitprint(io::IO, v::AbstractVector{Bool}) = print(io, join(Int8.(v), ""))
+function bitprint(io::IO, m::AbstractMatrix{Bool})
+    rows = eachrow(m)
+    n = length(rows)
+    for (i, r) in enumerate(rows)
+        bitprint(io, r)
+        if i != n
+            println(io)
+        end
+    end
+end
+
+
+"""
+    readhex(fname, ::Type{T}=Bin64) where T
+
+Read ascii hex-coded numbers into, and return, an array of eltype `T`.
+
+`T` can be, for example, `Bin64`, `UInt8` through `UInt128`, and `BitMatrix`.
+Each line of the file will be converted and appended to the output array.
+So, codes for a single output number may not be split across input lines.
+
+If `T` is `BitMatrix`, then the returned object is a `BitMatrix` with `64`
+columns.
+"""
 function readhex(fname, ::Type{T}=Bin64) where T
+    T <: BitMatrix && return bitmatrix(readhex(fname, UInt64))
     a = Vector{T}()
     open(fname) do f
         foreach(line -> append!(a, parsehexline(line, T)), readlines(f))
     end
     return a
 end
-
 
 # These explict reinterpretations are slightly faster than using the generic builtin reinterpret
 
@@ -51,7 +112,13 @@ end
 @inline touint(::Type{UInt64}, v) = toUInt64(v)
 @inline touint(::Type{Bin64}, v) = Bin64(touint(UInt64, v))
 
-@inline toUInt16(v::Union{AbstractVector{UInt8}, NTuple{2, UInt8}}) = shft(UInt16(v[1]), 1) | UInt16(v[2])
+# When Julia reinterprets, the first input byte is the least significant
+# In contrast, in our functions below, the first byte is the most significant.
+# These are often faster as well, because they return a single integer rather than an array.
+
+@inline function toUInt16(v::Union{AbstractVector{UInt8}, NTuple{2, UInt8}})
+    return shft(UInt16(v[1]), 1) | UInt16(v[2])
+end
 
 @inline function toUInt32(v::Union{AbstractVector{UInt8}, NTuple{4, UInt8}})
     return shft(UInt32(v[1]), 3) | shft(UInt32(v[2]), 2) |
@@ -65,14 +132,22 @@ end
         shft(UInt64(v[7]), 1) | UInt64(v[8])
 end
 
-function _reinterp(::Type{T}, bytes, start, stop) where {T <: Union{UInt16, UInt32, UInt64}}
+@inline function _reinterp(::Type{T}, bytes, start, stop) where {T <: Union{UInt16, UInt32, UInt64}}
     return touint(T, @view(bytes[start:stop]))
 end
 
-function _reinterp(::Type{T}, bytes, start, stop) where {T}
+# For UInt128, etc. we have not written a special purpose routine. So call Julia routines.
+@inline function _reinterp(::Type{T}, bytes, start, stop) where {T}
     return only(reinterpret(T, @view(bytes[stop:-1:start])))
 end
 
+"""
+    parsehexline(s::AbstractString, ::Type{T}=Bin64) where T
+
+Convert the hex-coded numbers in `s` to an array of numbers. In contrast
+to `reinterpret` in Julia, the order of the bits in the line is preserved
+in the output array.
+"""
 function parsehexline(s::AbstractString, ::Type{T}=Bin64) where T
     bytes = hex2bytes(s)
     T <: UInt8 && return bytes
@@ -119,7 +194,7 @@ function writehex(io::IO, n::UInt64)
 end
 
 """
-    eachrow(v, col_num)
+    eachrow(v::AbstractVector{<:Bin64}, col_num)
 
 Return an iterator over the `col_num`th column of the vector `v`.
 
