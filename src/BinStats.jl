c57c375f14d4679d830e2969d9d3164c741c241d
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-04-21 22:23:48 -0400

    Fix order of bytes

--- src/BinStats.jl
+++ src/BinStats.jl
@@ -4,13 +4,15 @@ import Random
 
 export readhex, writehex
 export Bin64
+export revbits
 
 struct Bin64
     x::UInt64
 end
 
 function Base.show(io::IO, b::Bin64)
-    print(io, reverse(string(b.x, base=2, pad=64)))
+    print(io, string(b.x, base=2, pad=64))
+#    print(io, reverse(string(b.x, base=2, pad=64)))
 end
 
 function Base.show(io::IO, v::Vector{<:Bin64})
@@ -23,7 +25,7 @@ end
 Random.rand(rng::Random.AbstractRNG, ::Random.SamplerType{Bin64}) = Bin64(rand(rng, UInt64))
 
 function Base.getindex(b::Bin64, i::Integer)
-    return Bool((b.x >> (i - 1)) & 1)
+    return Bool((b.x << (i - 1)) & 1)
 end
 
 function Base.getindex(b::Bin64, inds::AbstractVector{<:Integer})
@@ -33,9 +35,54 @@ end
 function readhex(fname, ::Type{T}=Bin64) where T
     a = Vector{T}()
     open(fname) do f
-        foreach(line -> append!(a, reinterpret(T, hex2bytes(line))), readlines(f))
+        foreach(line -> append!(a, parsehexline(line, T)), readlines(f))
     end
     return a
+end
+
+
+# These explict reinterpretations are slightly faster than using the generic builtin reinterpret
+
+@inline shft(x, n) = x << (8 * n)
+@inline touint(::Type{T}, v::UInt8...) where T = touint(T, v)
+@inline touint(::Type{UInt16}, v) = toUInt16(v)
+@inline touint(::Type{UInt32}, v) = toUInt32(v)
+@inline touint(::Type{UInt64}, v) = toUInt64(v)
+@inline touint(::Type{Bin64}, v) = Bin64(touint(UInt64, v))
+
+@inline toUInt16(v::Union{AbstractVector{UInt8}, NTuple{2, UInt8}}) = shft(UInt16(v[1]), 1) | UInt16(v[2])
+
+@inline function toUInt32(v::Union{AbstractVector{UInt8}, NTuple{4, UInt8}})
+    return shft(UInt32(v[1]), 3) | shft(UInt32(v[2]), 2) |
+        shft(UInt32(v[3]), 1) | UInt32(v[4])
+end
+
+@inline function toUInt64(v::Union{AbstractVector{UInt8}, NTuple{8, UInt8}})
+    shft(UInt64(v[1]), 7) | shft(UInt64(v[2]), 6) |
+        shft(UInt64(v[3]), 5) | shft(UInt64(v[4]), 4) |
+        shft(UInt64(v[5]), 3) | shft(UInt64(v[6]), 2) |
+        shft(UInt64(v[7]), 1) | UInt64(v[8])
+end
+
+function _reinterp(::Type{T}, bytes, start, stop) where {T <: Union{UInt16, UInt32, UInt64}}
+    return touint(T, @view(bytes[start:stop]))
+end
+
+function _reinterp(::Type{T}, bytes, start, stop) where {T}
+    return only(reinterpret(T, @view(bytes[stop:-1:start])))
+end
+
+function parsehexline(s::AbstractString, ::Type{T}=Bin64) where T
+    bytes = hex2bytes(s)
+    T <: UInt8 && return bytes
+    sizeT = sizeof(T)
+    v = Vector{T}(undef, div(length(bytes), sizeT))
+    for i in 1:length(v)
+        stop = sizeT * i
+        start = stop - sizeT + 1
+        v[i] = _reinterp(T, bytes, start, stop)
+    end
+    return v
 end
 
 """
@@ -64,7 +111,7 @@ end
 
 writehex(io::IO, b::Bin64) = writehex(io, b.x)
 function writehex(io::IO, n::UInt64)
-    v = reinterpret(UInt8, [n])
+    v = reverse!(reinterpret(UInt8, [n]))
     for x in v
         print(io, uppercase(string(x, base=16, pad=2)))
     end
@@ -95,20 +142,5 @@ julia> count(isone, eachrow(v, 2))
 ```
 """
 Base.eachrow(v::AbstractVector{<:Bin64}, col_num) = (x[col_num] for x in v)
-
-# Following it perhaps not useful
-# """
-#     oncol(f, i)
-
-# Return a function that calls `f` on the `i` element of
-# its input
-
-# # Example
-# Count the number of ones in column 2
-# ```juliarepl
-# julia> count(oncol(isone, 2), v)
-# ```
-# """
-# oncol(f, i) = b -> f(b[i])
 
 end # module BinStats
